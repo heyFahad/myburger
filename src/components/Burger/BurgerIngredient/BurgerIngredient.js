6d8cf3e9f6d8d6e4fab2be018965e37a78ead9f0
Fahad Javed <32978752+heyFahad@users.noreply.github.com>
2019-08-05 23:16:49 +0500

    Added dynamic BurgerIngredient components.

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import cssClasses from './BurgerIngredient.css';
+
+const BurgerIngredient = (props) => {
+    let ingredients = null;
+
+    switch (props.type) {
+        case 'bread-bottom':
+            ingredients = <div className={cssClasses.BreadBottom}></div>;
+            break;
+        
+        case 'bread-top':
+                ingredients = (
+                    <div className={cssClasses.BreadTop}>
+                        <div className={cssClasses.Seeds1}></div>
+                        <div className={cssClasses.Seeds1}></div>
+                    </div>
+                );
+                break;
+        
+        case 'meat':
+                ingredients = <div className={cssClasses.Meat}></div>;
+                break;
+        
+        case 'cheese':
+                ingredients = <div className={cssClasses.Cheese}></div>;
+                break;
+        
+        case 'salad':
+                ingredients = <div className={cssClasses.Salad}></div>;
+                break;
+        
+        case 'bacon':
+                ingredients = <div className={cssClasses.Bacon}></div>;
+                break;
+        
+        default:
+            break;
+    }
+
+    return ingredients;
+}
+
+export default BurgerIngredient;
