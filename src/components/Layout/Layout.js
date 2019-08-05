f4dfbd45bb9b974289d066c5f925c3c289418158
Fahad Javed <32978752+heyFahad@users.noreply.github.com>
2019-08-05 21:51:59 +0500

    Started implemention of the BurgerBuilder container.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import cssClasses from './Layout.css';
 
 const Layout = (props) => {
     return (
         <React.Fragment>
             <div>Toolbar, Sidedrawer, Backdrop</div>
-            <main>
+            <main className={cssClasses.Content}>
                 {props.children}
             </main>
         </React.Fragment>
