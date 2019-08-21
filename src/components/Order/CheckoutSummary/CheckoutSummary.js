c4ff21722368a82367e51823c0e750d5cbf02ed0
Fahad Javed <32978752+heyFahad@users.noreply.github.com>
2019-08-21 20:36:02 +0500

    Now adding Routing in our application. For that, I created the Checkout container first of all in this commit.

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import cssClasses from './CheckoutSummary.css';
+
+const CheckoutSummary = (props) => {
+    return (
+        <div className={cssClasses.CheckoutSummary}>
+            <h1>We hope this tastes well!</h1>
+            <div style={{ width: '100%', margin: 'auto' }}>
+                <Burger ingredients={props.ingredients} />
+            </div>
+            <Button
+                btnType="Danger"
+                btnClicked>
+                CANCEL
+            </Button>
+            <Button
+                btnType="Success"
+                btnClicked>
+                CONTINUE
+            </Button>
+        </div>
+    );
+}
+
+export default CheckoutSummary;
