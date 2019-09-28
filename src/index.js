1ed4166e41a40622f12e810615350faa7dc90d59
Fahad Javed <32978752+heyFahad@users.noreply.github.com>
2019-09-28 20:03:13 +0500

    Combined our two Reducers into one rootReducer.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import burgerBuilderReducer from './redux-store/reducers/burgerBuilder';
+import orderReducer from './redux-store/reducers/order';
 import * as serviceWorker from './serviceWorker';
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+});
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
