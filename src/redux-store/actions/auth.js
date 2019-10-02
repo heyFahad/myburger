d32d8641b29ed3b9845d7336ea39279080401584
Fahad Javed <32978752+heyFahad@users.noreply.github.com>
2019-10-02 17:31:16 +0500

    Getting an Auth Token from the backend.

--- src/redux-store/actions/auth.js
+++ src/redux-store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 const authStart = () => {
@@ -23,5 +24,20 @@ const authFailed = (error) => {
 export const auth = (email, password) => {
     return (dispatch) => {
         dispatch(authStart());
+
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCtiJ3uFK6tQEnUhlxidYAX_XTFbTrSTuo', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(authFailed(error));
+            });
     };
 };
